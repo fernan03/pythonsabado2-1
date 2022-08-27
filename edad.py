d6f5c5ed78364c3a4c5d99d31754af235d786746
luis <luisfernandosucerquia711@gmail.com>
2022-08-26 21:53:30 -0500

    NTS se completo los retos de edad y calendario

--- edad.py
+++ edad.py
@@ -1,14 +1,20 @@
-edad=int(input('DIgita tu edad: '))
 
-if(edad<0 or edad>=150):
-    print(f'usted no existe')
-elif(edad>=0 and edad<=14):
-    print(f'usted tiene {edad} eres niño')
-elif(edad>=15 and edad<28):
-    print(f'usted tiene {edad} eres adulto')
-elif(edad>28 and edad<=60):
-    print(f'usted tiene {edad} eres adulto')
-elif(edad>60):
-    print(f'Usted tiene {edad} eres mayor')
+
+while True:
+    try:
+        edad=int(input('DIgita tu edad: '))
+        if(edad<0 or edad>=150):
+            print(f'usted no existe')
+        elif(edad>=0 and edad<=14):
+            print(f'usted tiene {edad} eres niño')
+        elif(edad>=15 and edad<28):
+            print(f'usted tiene {edad} eres adulto')
+        elif(edad>28 and edad<=60):
+            print(f'usted tiene {edad} eres adulto')
+        elif(edad>60):
+            print(f'Usted tiene {edad} eres mayor')
+        break
+    except ValueError:
+        print("Valor no valido ")
 
 #Validar que sea necesario un numero
--- estaciones.py
+++ estaciones.py
@@ -1,4 +1,5 @@
 #ENTRADA DE PROBLEMA
+'''
 mes=input('Digite mes del año')
 
 #PROCESO 
@@ -12,6 +13,68 @@ elif (mes=="octubre" or mes=="noviembre" or mes=="diciembre"):
     print(f'Estas en {mes} es otoño')
 else:
     print('Digite mes valido')
+''' 
 
 #Como hacer para qe siempre digite en minuscula
 #hacer lo mismo pero con dias(datos de entrada:dias y mes)
+
+hemisferio=input("Digite en cual hemisferio esta(sur o norte): ")
+mes=input("Digite mes del año: ")
+dia=int(input("Digite dia: "))
+
+if(hemisferio == "sur"):
+    if(mes=="enero" or mes=="febrero"):
+        print(f'esta en verano')
+    elif(mes=="marzo" and dia<=20):
+        print(f'es en verano')
+    elif(mes=="marzo" and dia>=21):
+        print(f'esta en otoño')
+    elif(mes=="abril" or mes=="mayo"):
+        print(f'esta en otoño')
+    elif(mes=="junio" and dia<=20):
+        print(f'estas en otoño')
+    elif(mes=="junio" and dia>=21):
+        print(f'estas en invierno')
+    elif(mes=="julio" or mes=="agosto"):
+        print(f'estas en invierno')
+    elif(mes=="septiembre" and dia<=20):
+        print(f'estas en invierno')
+    elif(mes=="septiembre" and dia>=21):
+        print(f'estas en primavera')
+    elif(mes=="octubre" or mes=="noviembre"):
+        print(f'estas en primavera')
+    elif(mes=="diciembre" and dia<=20): 
+        print(f'estas en primavera')
+    elif(mes=="diciembre" and dia>=21):
+        print(f'estas en verano')
+    else:
+        print(f'digite datos validos')
+elif(hemisferio == "norte"):
+    if(mes=="enero" or mes=="febrero"):
+        print(f'esta en invierno')
+    elif(mes=="marzo" and dia<=20):
+        print(f'es en invierno')
+    elif(mes=="marzo" and dia>=21):
+        print(f'esta en primavera')
+    elif(mes=="abril" or mes=="mayo"):
+        print(f'esta en primavera')
+    elif(mes=="junio" and dia<=20):
+        print(f'estas en primavera')
+    elif(mes=="junio" and dia>=21):
+        print(f'estas en verano')
+    elif(mes=="julio" or mes=="agosto"):
+        print(f'estas en verano')
+    elif(mes=="septiembre" and dia<=20):
+        print(f'estas en verano')
+    elif(mes=="septiembre" and dia>=21):
+        print(f'estas en otoño')
+    elif(mes=="octubre" or mes=="noviembre"):
+        print(f'estas en otoño')
+    elif(mes=="diciembre" and dia<=20):
+        print(f'estas en otoño')
+    elif(mes=="diciembre" and dia>=21):
+        print(f'estas en invierno')
+    else:
+        print(f'digite datos validos')
+else:
+    print(f'digite datos validos')
